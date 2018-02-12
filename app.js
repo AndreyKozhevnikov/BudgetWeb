48e6cc8c1308d946fcc3993fc105ae6d31cc5c7f
Andrey <freazeek@mail.ru>
2018-02-12 12:59:26 +0300

    add favicon

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use('/', index);
 app.use('/users', users);
 app.use('/wiki', wiki);
