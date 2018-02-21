abd36d0807e2c7e374e247fc40d1705d8d23220f
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-02-21 10:06:26 +0300

    enable submit button back

--- public/javascripts/disableButton.js
+++ public/javascripts/disableButton.js
@@ -1,11 +1,10 @@
-console.log('my test string');
 window.onload=init;
 function init(){
    let btn=  document.querySelector('.btnDisableAfterClick');
    btn.addEventListener('click',disableOnSubmit);
-
 }
 
 function disableOnSubmit(){
 	setTimeout(()=>(this.disabled=true),1);
+	setTimeout(()=>(this.disabled=false),1000);
 }
