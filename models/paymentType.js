0f402b3374ca44452e969a851609f87b22428ea7
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2018-10-22 17:19:27 +0300

    modify PaymentType

--- models/paymentType.js
+++ models/paymentType.js
@@ -8,6 +8,7 @@ let PaymentTypeSchema = new Schema({
   Name: { type: String, max: 100, required: true },
   CurrentCount: { type: Number, required: true },
   IsYandex: { type: Boolean },
+  LocalId: {type: Number, required: true},
 });
 // Virtual for author's URL
 PaymentTypeSchema.virtual('url').get(function() {
