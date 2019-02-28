82c64c52a247e12db82a9976f19727e5dd623f51
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2019-02-28 08:49:03 +0300

    swap accin and accout

--- public/javascripts/base_list.js
+++ public/javascripts/base_list.js
@@ -1,6 +1,16 @@
 /*eslint no-unused-vars: ["off"]*/
 'use strict';
 function buildContainerForSOrder(container, sOrder, document) {
+  if (sOrder.AccountOut) {
+    let lblOutAccount = document.createElement('label');
+    lblOutAccount.classList.add('OutAccountLabel');
+    lblOutAccount.classList.add('plainLabel');
+    lblOutAccount.innerHTML = sOrder.AccountOut.Name;
+    container[0].appendChild(lblOutAccount);
+    
+    let br = document.createElement('br');
+    container[0].appendChild(br);
+  }
   if (sOrder.AccountIn) {
     let lblInAccount = document.createElement('label');
     lblInAccount.classList.add('InAccountLabel');
@@ -11,15 +21,5 @@ function buildContainerForSOrder(container, sOrder, document) {
     }
     lblInAccount.innerHTML = labelString;
     container[0].appendChild(lblInAccount);
-
-    let br = document.createElement('br');
-    container[0].appendChild(br);
-  }
-  if (sOrder.AccountOut) {
-    let lblOutAccount = document.createElement('label');
-    lblOutAccount.classList.add('OutAccountLabel');
-    lblOutAccount.classList.add('plainLabel');
-    lblOutAccount.innerHTML = sOrder.AccountOut.Name;
-    container[0].appendChild(lblOutAccount);
   }
 }
