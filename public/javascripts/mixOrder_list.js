9c2916153ca57b1157ca2d5bd17db3349446cc24
Andrey Kozhevnikov <andrey.kozhevnikov@devexpress.com>
2019-03-15 08:26:51 +0300

    format mixorder list

--- public/javascripts/mixOrder_list.js
+++ public/javascripts/mixOrder_list.js
@@ -28,12 +28,18 @@ window.onload = function() {
           dataField: 'value',
           width: '19%',
           cellTemplate: (container, options) => {
-            let a = document.createElement('a');
-            a.classList.add('dx-link');
-            a.text = options.data.value;
-            a.href = options.data.url;
-            a.style.wordWrap = 'break-word';
-            container[0].appendChild(a);
+            switch (options.data.type) {
+              case 'fixrecord':
+                container[0].innerHTML = '[' + options.data.value + ']';
+                break;
+              default:
+                let a = document.createElement('a');
+                a.classList.add('dx-link');
+                a.text = options.data.value;
+                a.href = options.data.url;
+                a.style.wordWrap = 'break-word';
+                container[0].appendChild(a);
+            }
           },
         },
         {
@@ -42,14 +48,24 @@ window.onload = function() {
         {
           dataField: 'data',
           cellTemplate: (container, options) => {
-            if (options.data.type === 'order') {
-              let lb = this.document.createElement('label');
-              lb.innerHTML = options.data.entity.ParentTag.Name;
-              lb.classList.add('plainLabel');
-              container[0].appendChild(lb);
-            } else {
-              buildContainerForSOrder(container, options.data.entity, this.document);
-              container[0].classList.add('tdSOrder');
+            switch (options.data.type) {
+              case 'order':
+                let lb = this.document.createElement('label');
+                lb.innerHTML = options.data.entity.ParentTag.Name;
+                lb.classList.add('plainLabel');
+                container[0].appendChild(lb);
+                break;
+              case 'serviceOrder':
+                buildContainerForSOrder(container, options.data.entity, this.document);
+                container[0].classList.add('tdSOrder');
+                break;
+              case 'fixrecord':
+                let lbf = this.document.createElement('label');
+                lbf.innerHTML = options.data.description;
+                lbf.classList.add('plainLabel');
+                container[0].classList.add('tdFixRecord');
+                container[0].appendChild(lbf);
+                break;
             }
           },
         },
