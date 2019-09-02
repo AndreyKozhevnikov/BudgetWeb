270f41288bf0d5d7feda481e259a3dd40b9ffa38
Andrey <freazeek@mail.ru>
2019-09-02 14:42:11 +0300

    temp

--- controllers/helperController.js
+++ controllers/helperController.js
@@ -52,8 +52,8 @@ function getFirstDayOfLastMonth() {
 }
 
 function getToday() {
-  let dt = new Date();
-  // let dt = new Date('2019-02-28');
+ // let dt = new Date();
+   let dt = new Date('2019-02-28');
   return dt;
 }
 
