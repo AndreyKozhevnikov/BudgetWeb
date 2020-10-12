8bf101db5fbdbee554e4ca73c5bdb73b0d6bc5ed
Andrey <andrey.kozhevnikov@devexpress.com>
2020-10-12 19:26:57 +0300

    add chart

--- public/javascripts/account_dates_chart.js
+++ public/javascripts/account_dates_chart.js
@@ -1,18 +1,34 @@
-/*global DevExpress thisMonthDates buildValueContainerForOrder*/
+/*global DevExpress thisMonthDates $ buildValueContainerForOrder*/
 /*eslint no-new: 0, new-cap: 0*/
 'use strict';
 
 window.onload = function() {
-  let chartElement = document.getElementById('thisMonthDatesContainer');
-  new DevExpress.ui.dxDataGrid(chartElement, {
-    dataSource: thisMonthDates,
-    series: {
+  // let chartElement = document.getElementById('thisMonthDatesContainer');
+  // new DevExpress.ui.dxChart(chartElement, {
+  console.log('test111');
+  $('#thisMonthDatesContainer').dxChart({
+    dataSource: Object.values(thisMonthDates),
+    series: [{
       argumentField: 'Date',
-      valueField: 'oranges',
-      name: 'My oranges',
+      valueField: 'Diff',
       type: 'bar',
       color: '#ffaa66',
+      axis: 'Diff',
     },
+    {
+      argumentField: 'Date',
+      valueField: 'TempResult',
+      color: '#ffaa66',
+      axis: 'Running',
+    }],
+    valueAxis: [{
+      name: 'Diff',
+      position: 'left',
+    }, {
+      name: 'Running',
+      position: 'right',
+    },
+    ],
   });
 
 };
