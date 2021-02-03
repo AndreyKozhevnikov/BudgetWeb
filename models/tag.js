e46fe1220f5a6a3a1ec8f3c3dd23ff1e6bf11e55
Andrey <andrey.kozhevnikov@devexpress.com>
2021-02-03 20:22:36 +0300

    change tag's url

--- models/tag.js
+++ models/tag.js
@@ -17,7 +17,7 @@ let TagSchema = new Schema({
 
 // Virtual for author's URL
 TagSchema.virtual('url').get(function() {
-  return '/tag/' + this._id;
+  return '/tag/' + this._id + '/update';
 });
 
 // Export model
