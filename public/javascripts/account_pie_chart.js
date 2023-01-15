02eff3d9dd7a5a503c35c14b2efa39b8e6cafd74
andrey <andrey.kozhevnikov@devexpress.com>
2023-01-15 13:01:20 +0400

    use one place to create both pie charts

--- public/javascripts/account_pie_chart.js
+++ public/javascripts/account_pie_chart.js
@@ -1,15 +1,11 @@
-/*global thisMonthSpendGroups  $ */
+/*global thisMonthSpendGroups lastWeekSpendGroups $ */
 /*eslint no-new: 0, new-cap: 0*/
 'use strict';
 // console.log('pie');
 let oldonload = window.onload;
-window.onload = function() {
-  oldonload();
-  //  let chartElement = document.getElementById('thisMonthPieChartContainer');
-  //  console.log(chartElement);
-  // new DevExpress.ui.dxChart(chartElement, {
-  $('#thisMonthPieChartContainer').dxPieChart({
-    dataSource: Object.values(thisMonthSpendGroups),
+function createPieChart(divName, collection){
+  $(divName).dxPieChart({
+    dataSource: Object.values(collection),
     palette: 'bright',
     series: [
       {
@@ -36,5 +32,15 @@ window.onload = function() {
       }
     },
   });
+}
+
+
+window.onload = function() {
+  oldonload();
+  //  let chartElement = document.getElementById('thisMonthPieChartContainer');
+  //  console.log(chartElement);
+  // new DevExpress.ui.dxChart(chartElement, {
+  createPieChart('#thisMonthPieChartContainer', thisMonthSpendGroups);
+  createPieChart('#lastWeekPieChartContainer', lastWeekSpendGroups);
 
 };
