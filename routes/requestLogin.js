2030839c99fedf8e696aa4d7000f354dcd4f0181
AndreyKozhevnikov <andrey.kozhevnikov@devexpress.com>
2023-04-22 15:17:06 +0400

    Update requestLogin.js

--- routes/requestLogin.js
+++ routes/requestLogin.js
@@ -10,7 +10,7 @@ function authenticate(name, pass, req, res, next, succesAuthentificate, id) {
     pass,
     function(error, user) {
       if (error || !user) {
-        let err = new Error('wrong name or pass');
+        let err = new Error('wrong name or pass'+user);
         err.status = 401;
         return next(err);
       } else {
