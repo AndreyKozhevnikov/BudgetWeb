0725dd7a9673d6002ab30230f2a26e69b33eb2f0
andrey <andrey.kozhevnikov@devexpress.com>
2025-03-21 20:04:15 +0400

    configure eslint

--- routes/fixRecordRouter.js
+++ routes/fixRecordRouter.js
@@ -1,12 +1,16 @@
-'use strict';
-let express = require('express');
-let router = express.Router();
-let fixRecord_controller = require('../controllers/fixRecordController.js');
 
-router.get('/createTotalSums', fixRecord_controller.createTotalSums);
-router.get('/deleteStartMonthRecords', fixRecord_controller.deleteStartMonthRecords);
-router.get('/showTotalSumsChart', fixRecord_controller.showTotalSumsChart);
-router.get('/createTotalIncoming', fixRecord_controller.createTotalIncoming);
-router.get('/removeTotals', fixRecord_controller.removeTotals);
+'use strict'
+let express = require('express')
+let router = express.Router()
+let fixRecord_controller = require('../controllers/fixRecordController.js')
 
-module.exports = router;
+router.get('/createTotalSums', fixRecord_controller.createTotalSums)
+router.get(
+    '/deleteStartMonthRecords',
+    fixRecord_controller.deleteStartMonthRecords,
+)
+router.get('/showTotalSumsChart', fixRecord_controller.showTotalSumsChart)
+router.get('/createTotalIncoming', fixRecord_controller.createTotalIncoming)
+router.get('/removeTotals', fixRecord_controller.removeTotals)
+
+module.exports = router
