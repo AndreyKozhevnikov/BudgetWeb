e8ec03292b32575215fc188f8d930016d01e274d
andrey <andrey.kozhevnikov@devexpress.com>
2025-04-24 15:58:13 +0400

    Revert "Merge pull request #29 from AndreyKozhevnikov/develop"
    This reverts commit 5e8b84041080873fa25e7e6710ca55183136cb31, reversing
    changes made to c7c55e65dc5d866a9bc6097ebd0424fbbd1e417b.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 'use strict'
-require('dotenv').config();
 let express = require('express')
 let path = require('path')
 let favicon = require('serve-favicon')
@@ -47,9 +46,8 @@ app.use(
     }),
 )
 let isDevelopment = process.env.NODE_ENV === 'development'
-// isDevelopment = false;
-// if (!isDevelopment)
-app.use('/', requestLogin)
+// let isDevelopment = false;
+if (!isDevelopment) app.use('/', requestLogin)
 app.use('/', rootRouter)
 app.use('/tag', tagRouter)
 app.use('/orderPlace', orderPlaceRouter)
@@ -62,7 +60,7 @@ app.use('/fixRecord', fixRecordRouter)
 // Set up mongoose connection
 let mongoose = require('mongoose')
 
-let mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/budgetwebdb'
+let mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/budgetWebTest'
 // let mongoDB = 'mongodb://127.0.0.1:27017/budgetwebdb'
 
 console.log(mongoDB)
